ac1a99ace6544a7a15f9e81bbef428b94c6ff296
Judd Baroff <95006227+GearLion@users.noreply.github.com>
2022-01-04 17:28:36 -0600

    updated to allow repeat play and track high score

--- main.py
+++ main.py
@@ -16,36 +16,48 @@ snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
-screen.listen()
-screen.onkey(key="Up", fun=snake.go_up)
-screen.onkey(key="Down", fun=snake.go_down)
-screen.onkey(key="Left", fun=snake.go_left)
-screen.onkey(key="Right", fun=snake.go_right)
-
 # Game Proper
 game_running = True
 while game_running:
+
+    screen.listen()
+    screen.onkey(key="Up", fun=snake.go_up)
+    screen.onkey(key="Down", fun=snake.go_down)
+    screen.onkey(key="Left", fun=snake.go_left)
+    screen.onkey(key="Right", fun=snake.go_right)
+
     screen.update()
     time.sleep(0.1)
 
     snake.move()
 
     # Detect collision with food
-    if snake.head.distance(food) < 13:
+    if snake.head.distance(food) < 15:
         food.reappear()
+        scoreboard.snake_scores()
         scoreboard.keep_score()
         snake.grow()
 
     # Detect collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         scoreboard.game_over()
-        game_running = False
+        scoreboard.is_high_score()
+        if scoreboard.play_again():
+            snake.reset_snake()
+            scoreboard.keep_score()
+        else:
+            game_running = False
 
     # Detect collision with self
     for segment in snake.body[1:]:
-        if snake.head.distance(segment) < 13:
+        if snake.head.distance(segment) < 15:
             scoreboard.game_over()
-            game_running = False
+            scoreboard.is_high_score()
+            if scoreboard.play_again():
+                snake.reset_snake()
+                scoreboard.keep_score()
+            else:
+                game_running = False
 
 
 screen.exitonclick()
